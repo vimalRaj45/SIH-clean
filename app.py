1ba17989910a6697a9279dcdfbeda94a346b33c3
vimalRaj45 <vimalraj5207@gmail.com>
2025-09-27 08:39:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,5 @@ def search():
     return jsonify([dict(r) for r in results])
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
 
